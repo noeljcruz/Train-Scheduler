260bbffa3316b450bb727ed2d43ca691ca4872ff
Noel Cruz <noel.j.cruz@outlook.com>
2019-07-30 00:49:08 -0700

    Continued working on JS. Still working on persistence.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,21 +12,6 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-var connectionsRef = database.ref("/connections");
-var connectedRef = database.ref(".info/connected");
-
-connectedRef.on("value", function (snap) {
-
-    if (snap.val()) {
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
-    }
-});
-
-connectionsRef.on("value", function (snapshot) {
-
-    $("#train-table > tbody").text(snapshot.numChildren());
-});
 
 $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
@@ -52,24 +37,20 @@ $("#add-train-btn").on("click", function (event) {
     $("#first-train-input").val("");
     $("#frequency-input").val("");
 
-    database.ref().on("child_added", function (childSnapshot) {
+    database.ref().on("child_added", renderSchedule);
+
+    function renderSchedule(childSnapshot) {
 
         var trainName = childSnapshot.val().name;
         var trainDestination = childSnapshot.val().destination;
         var firstTrain = childSnapshot.val().first;
 
         var frequency = parseInt(frequency);
-
         var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-
         var currentTime = moment();
-
         var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
-
         var remainingTime = diffTime % trainFrequency;
-
         var minutesAway = trainFrequency - remainingTime;
-
         var nextTrain = moment().add(minutesAway, "minutes");
 
         var newRow = $("<tr>").append(
@@ -81,5 +62,5 @@ $("#add-train-btn").on("click", function (event) {
         );
 
         $("#train-table > tbody").append(newRow);
-    });
+    }
 });
